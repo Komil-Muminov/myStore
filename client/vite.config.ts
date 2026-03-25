1c0afa6ae77ed6164c386a6a60292cf06e61b0cc
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2026-03-25 19:00:29 +0500

    fx

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,9 +2,21 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@assets": path.resolve(__dirname, "./src/assets"),
+			"@shared": path.resolve(__dirname, "./src/shared"),
+			"@entities": path.resolve(__dirname, "./src/entities"),
+			"@features": path.resolve(__dirname, "./src/features"),
+			"@widgets": path.resolve(__dirname, "./src/widgets"),
+			"@pages": path.resolve(__dirname, "./src/pages"),
+		},
+	},
 	plugins: [
 		react(),
 		babel({ presets: [reactCompilerPreset()] }),
